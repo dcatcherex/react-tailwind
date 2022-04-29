ffdeae92f22c614ffb6219bf2a64abe7ab86e67c
dcatcherex <dcatcherex@yahoo.com>
2022-04-29 14:48:41 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import logo from "./Assets/Logos/logo-dark.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="" />
+        <h1 className="mt-4">This is my App</h1>
+        มาเขียน code กันเถอะ
+      </header>
+    </div>
+  );
+}
+
+export default App;
